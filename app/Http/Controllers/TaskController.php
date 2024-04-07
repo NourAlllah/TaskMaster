955d919d5d4694c6017fb8f6fad50696eb2b37ec
NourAllah <nourabase1998@gmail.com>
2024-04-07 14:55:30 +0200

    solve the return from exampleTest unit test

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -30,8 +30,17 @@ class TaskController extends Controller
         $admins_id =  $this->typeModel->get_admins_id();
         $not_admins_id =  $this->typeModel->get_users_id();
 
-        $all_admins = $this->userModel->get_all_type_of_users($admins_id[0]->id);
-        $all_not_admins = $this->userModel->get_all_type_of_users($not_admins_id[0]->id);
+       if (count($admins_id) > 0) {
+            $all_admins = $this->userModel->get_all_type_of_users($admins_id[0]->id);
+       }else{
+            $all_admins = [];
+       }
+
+       if (count($not_admins_id) > 0) {
+            $all_not_admins = $this->userModel->get_all_type_of_users($not_admins_id[0]->id);
+        }else{
+            $all_not_admins = [];
+       }
 
         return view('create_task')->with(['admins'=> $all_admins , 'not_admins'=>$all_not_admins ]);
 
